d6743de85447a04edac79f722931fa3a05781e2f
Felipe de Azevedo <felipe.deazevedo@outlook.com>
2022-08-19 00:26:38 -0300

    Subindo implementações feitas até o momento.

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,47 @@
+package com.api.parkingcontrol.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+	final ParkingSpotRepository parkingSpotRepository;
+	
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+	public boolean existsByCarLicensePlate(String carLicensePlate) {
+		return parkingSpotRepository.existsByCarLicensePlate(carLicensePlate);
+	}
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+	public boolean existsByApartmentAndBlock(String apartment, String block) {
+		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+	}
+
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+}
